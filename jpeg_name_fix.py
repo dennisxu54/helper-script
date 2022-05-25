5606747c12e081a64c7f663ddda16ce9e25d47f4
unknown <dennisxu54@hotmail.com>
2022-05-25 16:38:19 +1200

    changed directory

--- jpeg_name_fix.py
+++ jpeg_name_fix.py
@@ -25,13 +25,11 @@ print("Current working directory: {0}".format(os.getcwd()))
 print("------------------------------------------------------------------------")
 folder_path  = os.chdir('C:/Work/Honeybee container/honeybee/products/management/data/reshoot colour')
 
-list1 = []
 
 for file in os.listdir(folder_path):
     file_name = file.split('.')[0].lower()
     file_name_list = file_name.split('_')
     last_part = file_name_list[-1]
-    """
     if len(file_name_list) >= 5:
         print("These products have need of colour name fix " + file_name)
     elif last_part == '01':
@@ -40,9 +38,7 @@ for file in os.listdir(folder_path):
         continue
     else:
         print("These products have no 01 or 02 " + file_name)
-    """
-    list1.append(file_name_list[0])
 
-print((list1))
+
     
 
