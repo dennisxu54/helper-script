2bf956b511f282d1192601432cfd93d8fb52191b
unknown <dennisxu54@hotmail.com>
2022-05-25 15:48:06 +1200

    This is to test random things

---
+++ testing.py
@@ -0,0 +1,13 @@
+sizes = {'sg407l': ['Sky', 'Sky', 'White', 'White', 'Black', 'Black', 'Red', 'Red', 'White', 'White', 'Lime', 'Lime', 'White', 'White', 'White', 'White'], 'sg407m': ['Black', 'Black', 'Gold', 'Red', 'Red', 
+'White', 'White', 'Lime', 'Lime', 'Sky', 'Sky', 'White', 'White', 'White', 'White', 'White', 'White'], 'sg702l': ['BlackGold', 'BlackGold', 'BlackRed', 'BlackRed', 'BlackTeal', 'BlackTeal', 'BlackWhite', 'BlackWhite', 'GreyBlackFluoroYellow', 'GreyBlackFluoroYellow', 'GreyFluoroLime', 'GreyFluoroLime', 'MagentaBlack', 'MagentaBlack', 'NavyRed', 'NavyRed', 'NavySky', 'NavySky', 'NavyWhite', 'NavyWhite', 'RedWhite', 'RedWhite', 'RoyalNavyFluoroOrange', 'RoyalNavyFluoroOrange', 'RoyalWhite', 'RoyalWhite', 'WhiteBlack', 'WhiteBlack'], 'sg702m': ['BlackGold', 'BlackGold', 'BlackRed', 'BlackRed', 'BlackTeal', 'BlackTeal', 'BlackWhite', 'BlackWhite', 'GreyBlackFluoroYellow', 'GreyBlackFluoroYellow', 'GreyFluoroLime', 'GreyFluoroLime', 'MagentaBlack', 'MagentaBlack', 'NavyRed', 'NavyRed', 
+'NavySky', 'NavySky', 'NavyWhite', 'NavyWhite', 'RedWhite', 'RedWhite', 'RoyalNavyFluoroOrange', 'RoyalNavyFluoroOrange', 'RoyalWhite', 'RoyalWhite', 'WhiteBlack', 'WhiteBlack'], 't406ks': ['Black', 'Black', 'BlackRed', 'BlackRed', 'BlackWhite', 'BlackWhite', 'GreyFluroLime', 'GreyLime', 'NavySky', 'NavyWhite', 'NavyWhite', 'Sky', 'RedWhite', 'White', 'RoyalWhite', 'RoyalWhite'], 't406ms': ['Black', 'Black', 'BlackRed', 'BlackRed', 'BlackWhite', 'BlackWhite', 'GreyFluroLime', 'GreyLime', 'NavySky', 'NavyWhite', 'NavyWhite', 'Sky', 'RedWhite', 'White', 'RoyalWhite', 'RoyalWhite']}
+
+
+keys_only = sizes.keys()
+
+for keys in keys_only:
+    print(keys)
+
+
+
+
