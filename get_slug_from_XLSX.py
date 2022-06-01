4817ee1801ce28d3597d5c815bc17b28d9f394a2
unknown <dennisxu54@hotmail.com>
2022-06-01 14:21:15 +1200

    fixed the script for the size update

--- get_slug_from_XLSX.py
+++ get_slug_from_XLSX.py
@@ -7,6 +7,8 @@ wrkbk = openpyxl.load_workbook("Biz Collection - Sizing.xlsx", data_only=True)
 
 sh = wrkbk["Sheet1"]
 
+size_names = ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL", "6XL", "7XL", "2", "4", "6", "8", "10", "12", "14", "16", 
+      "18", "20", "22", "24", "26", "28", "30", "72", "77", "82", "87", "92", "97", "102", "107", "112", "117", "122", "127", "132", "137", "142"]
 list1 = []
 myDict = {}
   
@@ -15,90 +17,67 @@ for i in range(2, 920):
     print("\n")
     print("Row ", i, " data :")
       
-    cell_product_slug = sh.cell(row=i, column=2).value
+    cell_product_size_name = sh.cell(row=i, column=2).value
     if cell_product_slug == " " or cell_product_slug == None or cell_product_slug == "-":
         continue
-    print(cell_product_slug)
-    #pro_slug = cell_product_slug.split(" ", 1)
-    #key = pro_slug[0].lower()
-    #key = str(cell_product_slug).lower() 
-    key = str(cell_product_slug)
-    if "Lemgth" in key:
-        key = key.replace("Lemgth", "Length")
-    if "lenght" in key:
-        key = key.replace("lenght", "Length")
-    if "Lenghth" in key:
-        key = key.replace("Lenghth", "Length")
+    print(cell_product_size_name)
+    key = str(cell_product_size_name)
     if "length" in key:
         key = key.replace("length", "Length")
-    if "Legth" in key:
-        key = key.replace("Legth", "Length")
     if "Lenth" in key:
         key = key.replace("Lenth", "Length")
     if "SLeeve" in key:
         key = key.replace("SLeeve", "Sleeve")
-    if "CENtre" in key:
-        key = key.replace("CENtre", "Centre")
     if "  " in key:
         key = key.replace("  ", " ")
-    if "form" in key:
-        key = key.replace("form", "From")
     if "from" in key:
         key = key.replace("from", "From")
-    if "fro" in key:
-        key = key.replace("fro", "From")
-    if "From From" in key:
-        key = key.replace("From From", "From")
-    if "HSP(CM)" in key:
-        key = key.replace("HSP(CM)", "HSP (CM)")
     if "CNB" in key:
         key = key.replace("CNB", "CBN")
     if "\n" in key:
         key = key.replace("\n", "")
-    if "back" in key:
-        key = key.replace("back", "Back")
     if " ( CM)" in key:
         key = key.replace(" ( CM)", " (CM)")
     if "(CM)" not in key:
         key = key + " (CM)"
     if "  (CM)" in key:
         key = key.replace("  (CM)", " (CM)")
-    if "FromHSP" in key:
-        key = key.replace("FromHSP", "From HSP")
-    if "FromLSP" in key:
-        key = key.replace("FromLSP", "From LSP")
-    if "Bdoy" in key:
-        key = key.replace("Bdoy", "Body")
     if "1/2" in key:
         key = key.replace("1/2", "½")
+        
+    if "Outleg From Waist (CM)" in key:
+        key = "Outer Leg (cm)"
+    if "Full Leg Length (CM)" in key:
+        key = "Leg Length (cm)"
+    if "Cut to Fit Sizes (CM)" in key:
+        key = "To Fit Sizes"
+    if "Inleg (CM)" in key or "Full Leg Length (CM)" in key:
+        key = "Leg Length (cm)"
+        
+    if "(CM)" in key:
+        key = key.replace("(CM)", "(cm)")
+    if "chest" in key:
+        key = key.replace("chest", "Chest")
 
+    size_list = []
+    index = 0
+    for x in range(3, 45):
+        cell_tier_price1 = sh.cell(row=i, column=x).value
+        if cell_tier_price1:
+            index += 1
+            size_list.append({"key": size_names[x-3], "value": str(cell_tier_price1), "position": index})
+            print({"key": size_names[x-3], "value": str(cell_tier_price1), "position": index})
+        else:
+            continue
 
-    #cell_tier_price1 = sh.cell(row=i, column=8).value
-    """
-    cell_tier_price1 = cell_tier_price1.lower()
-    cell_tier_price2 = sh.cell(row=i, column=8).value
-    cell_tier_price3 = sh.cell(row=i, column=9).value
-    
-    if cell_tier_price == None:
-        continue
-    if type(cell_tier_price) == float:
-        pass
-    else:
-        cell_tier_price = float(cell_tier_price[1:])
-    """
-    #value = [cell_tier_price, cell_tier_price*2]
-    #print(value)
-    """
-    if cell_tier_price1:
-        features = cell_tier_price1.split(";")
-    else:
-        features = ""
-    """
+    cell_product_slug = sh.cell(row=i, column=1).value
+    slug = str(cell_product_slug).lower() 
+    slug = str(cell_product_slug)
+        
+    myDict = {"size_details": size_list}
+    list1.append([key, size_list])
 
-    new_list_value = key
-    list1.append(new_list_value)
-
-#if you do want a unique list with no duplicates, remember to remove square brackets from above
-unique_list = list(dict.fromkeys(list1))
-print(unique_list)
-
+#if you do want a unique list with no duplicates
+#unique_list = list(dict.fromkeys(list1))
+#print(unique_list)
+print(list1)
--- testing.py
+++ testing.py
@@ -1,12 +1,9 @@
-sizes = {'sg407l': ['Sky', 'Sky', 'White', 'White', 'Black', 'Black', 'Red', 'Red', 'White', 'White', 'Lime', 'Lime', 'White', 'White', 'White', 'White'], 'sg407m': ['Black', 'Black', 'Gold', 'Red', 'Red', 
-'White', 'White', 'Lime', 'Lime', 'Sky', 'Sky', 'White', 'White', 'White', 'White', 'White', 'White'], 'sg702l': ['BlackGold', 'BlackGold', 'BlackRed', 'BlackRed', 'BlackTeal', 'BlackTeal', 'BlackWhite', 'BlackWhite', 'GreyBlackFluoroYellow', 'GreyBlackFluoroYellow', 'GreyFluoroLime', 'GreyFluoroLime', 'MagentaBlack', 'MagentaBlack', 'NavyRed', 'NavyRed', 'NavySky', 'NavySky', 'NavyWhite', 'NavyWhite', 'RedWhite', 'RedWhite', 'RoyalNavyFluoroOrange', 'RoyalNavyFluoroOrange', 'RoyalWhite', 'RoyalWhite', 'WhiteBlack', 'WhiteBlack'], 'sg702m': ['BlackGold', 'BlackGold', 'BlackRed', 'BlackRed', 'BlackTeal', 'BlackTeal', 'BlackWhite', 'BlackWhite', 'GreyBlackFluoroYellow', 'GreyBlackFluoroYellow', 'GreyFluoroLime', 'GreyFluoroLime', 'MagentaBlack', 'MagentaBlack', 'NavyRed', 'NavyRed', 
-'NavySky', 'NavySky', 'NavyWhite', 'NavyWhite', 'RedWhite', 'RedWhite', 'RoyalNavyFluoroOrange', 'RoyalNavyFluoroOrange', 'RoyalWhite', 'RoyalWhite', 'WhiteBlack', 'WhiteBlack'], 't406ks': ['Black', 'Black', 'BlackRed', 'BlackRed', 'BlackWhite', 'BlackWhite', 'GreyFluroLime', 'GreyLime', 'NavySky', 'NavyWhite', 'NavyWhite', 'Sky', 'RedWhite', 'White', 'RoyalWhite', 'RoyalWhite'], 't406ms': ['Black', 'Black', 'BlackRed', 'BlackRed', 'BlackWhite', 'BlackWhite', 'GreyFluroLime', 'GreyLime', 'NavySky', 'NavyWhite', 'NavyWhite', 'Sky', 'RedWhite', 'White', 'RoyalWhite', 'RoyalWhite']}
+sizes = ["XXS", "XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL", "6XL", "7XL", "2", "4", "6", "8", "10", "12", "14", "16", 
+      "18", "20", "22", "24", "26", "28", "30", "72", "77", "82", "87", "92", "97", "102", "107", "112", "117", "122", "127", "132", "137", "142"]
 
-
-keys_only = sizes.keys()
-
-for keys in keys_only:
-    print(keys)
+for x in range (3, 45):
+    print(sizes[x-3])
+print(len(sizes))
 
 
 
