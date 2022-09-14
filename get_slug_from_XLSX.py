911782ada04bf23dbec422cc11eadf4a566b4b5b
unknown <dennisxu54@hotmail.com>
2022-09-14 14:13:39 +1200

    updated script file for the biz collection new product page look

--- get_slug_from_XLSX.py
+++ get_slug_from_XLSX.py
@@ -3,20 +3,20 @@ import openpyxl
 import math
   
 # load excel with its path
-wrkbk = openpyxl.load_workbook("2022 Biz Collection_NZ Pricelist Reseller_01-2022.xlsx", data_only=True)
+wrkbk = openpyxl.load_workbook("Biz Collection - Product Details _Jodi.xlsx", data_only=True)
 
 # Make sure the sheet name is same as on the excel
-sh = wrkbk["Sheet1"]
+sh = wrkbk["HEALTH & BEAUTY"]
 
 list1 = []
 myDict = {}
   
 # iterate through excel and display data
-for i in range(3, 737):
+for i in range(2, 15):
     print("\n")
     print("Row ", i, " data :")
       
-    cell_product_slug = sh.cell(row=i, column=2).value
+    cell_product_slug = sh.cell(row=i, column=1).value
     if cell_product_slug == " " or cell_product_slug == None or cell_product_slug == "Style" or cell_product_slug == "STYLE":
         continue
     if cell_product_slug == "WORKS PANTS & SHORTS" or cell_product_slug == "ENGINEERED OUTERWEAR" or cell_product_slug == "OVERALLS" or cell_product_slug == "FIRE ARMOUR":
@@ -34,9 +34,16 @@ for i in range(3, 737):
     #key = str(cell_product_slug).lower() 
     #key = str(cell_product_slug)
 
-    cell_tier_price1 = sh.cell(row=i, column=7).value
-    #cell_tier_price2 = sh.cell(row=i, column=7).value
-    #cell_tier_price3 = sh.cell(row=i, column=8).value
+    description = sh.cell(row=i, column=3).value
+    description = description.replace('\n',"")
+    fabric = sh.cell(row=i, column=5).value
+    fabric_feature = sh.cell(row=i, column=6).value
+    fabric_care = sh.cell(row=i, column=7).value
+    size_range = sh.cell(row=i, column=8).value
+    size_fit = sh.cell(row=i, column=9).value
+    size_model = sh.cell(row=i, column=10).value
+    size_height = sh.cell(row=i, column=11).value
+    features = sh.cell(row=i, column=12).value
     """
     cell_tier_price1 = cell_tier_price1.lower()
     cell_tier_price2 = sh.cell(row=i, column=8).value
@@ -58,11 +65,10 @@ for i in range(3, 737):
         features = ""
     """
     
-    new_list_value = [pro_slug, cell_tier_price1]
+    new_list_value = [pro_slug, description, fabric, fabric_feature, fabric_care, size_range, size_fit, size_model, size_height, features]
     list1.append(new_list_value)
 
 #if you do want a unique list with no duplicates, remember to remove square brackets from above
 #unique_list = list(dict.fromkeys(list1))
 #print(unique_list)
 print(list1)
-
