993ce214a04b0038c9ff66e4fb5ee7c55bfc4b5b
unknown <dennisxu54@hotmail.com>
2023-04-27 14:21:22 +1200

    added in new script for getting the prices for products

---
+++ get_price_from_XLSX.py
@@ -0,0 +1,42 @@
+# import module
+import openpyxl
+import math
+  
+# load excel with its path
+wrkbk = openpyxl.load_workbook("2023 Biz Collection_NZ Pricelist Reseller.xlsx", data_only=True)
+
+# Make sure the sheet name is same as on the excel
+sh = wrkbk["NZ Pricing"]
+
+list1 = []
+myDict = {}
+  
+# iterate through excel and display data
+for i in range(2, 797):
+    print("\n")
+    print("Row ", i, " data :")
+    category_name = [" ", "Style", "STYLE", "WORKS PANTS & SHORTS", "ENGINEERED OUTERWEAR", "OVERALLS", "FIRE ARMOUR", "POLOS", "TEES", 
+                     "SHIRTS", "BIZ SEPARATES", "KNITWEAR", "HOODIES & FLEECE", "ACTIVEWEAR", "JACKETS", "HOSPITALITY", "HEALTH & BEAUTY",
+                     "HEALTHWEAR", "CASUALWEAR", "TOPS", "SEPARATES"]
+      
+    # Make sure this is the correct column i.e. the unique identifier
+    cell_product_slug = sh.cell(row=i, column=3).value
+    if (cell_product_slug == None or (cell_product_slug in category_name)):
+        continue
+    print(cell_product_slug)
+    pro_slug = cell_product_slug.strip()
+    pro_slug = pro_slug.lower()
+
+    price = sh.cell(row=i, column=7).value
+    
+    # This is so that there are no duplicates in the end list
+    new_list_value = [pro_slug, price]
+    if new_list_value in list1:
+        continue
+    else:
+        list1.append(new_list_value)
+
+# Below code is there to quickly eliminate duplicates from list if not using the above method
+# unique_list = list(dict.fromkeys(list1))
+# print(unique_list)
+print(list1)
