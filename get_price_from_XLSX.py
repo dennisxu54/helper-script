72f0a9f1e679d3f99ef353b15b8512fe9ce6ec10
Dennis <dennisxu54@hotmail.com>
2023-08-14 16:40:52 +1200

    updated scripts to get data from excel file

--- get_price_from_XLSX.py
+++ get_price_from_XLSX.py
@@ -3,16 +3,16 @@ import openpyxl
 import math
   
 # load excel with its path
-wrkbk = openpyxl.load_workbook("2023 Biz Collection_NZ Pricelist Distributor.xlsx", data_only=True)
+wrkbk = openpyxl.load_workbook("Sept 2023 Biz Collection_Biz Corporates USA Price List .xlsx", data_only=True)
 
 # Make sure the sheet name is same as on the excel
-sh = wrkbk["NZ Pricing"]
+sh = wrkbk["CAN"]
 
 list1 = []
 myDict = {}
   
 # iterate through excel and display data
-for i in range(4, 797):
+for i in range(3, 280):
     print("\n")
     print("Row ", i, " data :")
     category_name = [" ", "Style", "STYLE", "WORKS PANTS & SHORTS", "ENGINEERED OUTERWEAR", "OVERALLS", "FIRE ARMOUR", "POLOS", "TEES", 
@@ -20,19 +20,22 @@ for i in range(4, 797):
                      "HEALTHWEAR", "CASUALWEAR", "TOPS", "SEPARATES", 'PAGE']
       
     # Make sure this is the correct column i.e. the unique identifier
-    cell_product_slug = sh.cell(row=i, column=3).value
+    cell_product_slug = sh.cell(row=i, column=4).value
     if (cell_product_slug == None or (cell_product_slug in category_name)):
         continue
     print(cell_product_slug)
-    pro_slug = cell_product_slug.strip()
+    pro_slug = str(cell_product_slug).strip()
     pro_slug = pro_slug.lower()
 
-    price1 = sh.cell(row=i, column=7).value
-    price2 = sh.cell(row=i, column=8).value
-    price3 = sh.cell(row=i, column=9).value
+    price1 = sh.cell(row=i, column=8).value
+    price2 = sh.cell(row=i, column=9).value
+    price3 = sh.cell(row=i, column=10).value
+    price4 = sh.cell(row=i, column=11).value
+    # price5 = sh.cell(row=i, column=12).value
+    # price6 = sh.cell(row=i, column=13).value
     
     # This is so that there are no duplicates in the end list
-    new_list_value = [pro_slug, price1, price2, price3]
+    new_list_value = [pro_slug, price1, price2, price3, price4]
     if new_list_value in list1:
         continue
     else:
