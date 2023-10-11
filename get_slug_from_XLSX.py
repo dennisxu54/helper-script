48193ef4887440c9d52a7f59ddb87018b89cfb2f
Dennis <dennisxu54@hotmail.com>
2023-10-11 16:42:14 +1300

    updated the rename script to actually rename file names and getting data for v2 info on honeybee

--- get_slug_from_XLSX.py
+++ get_slug_from_XLSX.py
@@ -3,20 +3,20 @@ import openpyxl
 import math
   
 # load excel with its path
-wrkbk = openpyxl.load_workbook("Biz Collection - Product Details _Jodi_14.09.2022.xlsx", data_only=True)
+wrkbk = openpyxl.load_workbook("Fashion Biz AU NZ - Product Details.xlsx", data_only=True)
 
 # Make sure the sheet name is same as on the excel
-sh = wrkbk["HEALTH & BEAUTY"]
+sh = wrkbk["POLOS"]
 
 list1 = []
 myDict = {}
   
 # iterate through excel and display data
-for i in range(2, 120):
+for i in range(2, 205):
     print("\n")
     print("Row ", i, " data :")
       
-    cell_product_slug = sh.cell(row=i, column=1).value
+    cell_product_slug = sh.cell(row=i, column=3).value
     if cell_product_slug == " " or cell_product_slug == None or cell_product_slug == "Style" or cell_product_slug == "STYLE":
         continue
     if cell_product_slug == "WORKS PANTS & SHORTS" or cell_product_slug == "ENGINEERED OUTERWEAR" or cell_product_slug == "OVERALLS" or cell_product_slug == "FIRE ARMOUR":
@@ -28,36 +28,29 @@ for i in range(2, 120):
     if cell_product_slug == "HEALTHWEAR" or cell_product_slug == "CASUALWEAR" or cell_product_slug == "TOPS" or cell_product_slug == "SEPARATES":
         continue
     print(cell_product_slug)
-    pro_slug = cell_product_slug.strip()
+    pro_slug = str(cell_product_slug).strip()
     pro_slug = pro_slug.lower()
     #key = pro_slug[0].lower()
     #key = str(cell_product_slug).lower() 
     #key = str(cell_product_slug)
 
-    name = sh.cell(row=i, column=2).value
-    description = sh.cell(row=i, column=3).value
-    description = description.replace('\n',"")
-    fabric = sh.cell(row=i, column=5).value
-    fabric_feature = sh.cell(row=i, column=6).value
-    fabric_care = sh.cell(row=i, column=7).value
-    size_range = sh.cell(row=i, column=8).value
-    size_fit = sh.cell(row=i, column=9).value
-    size_model = sh.cell(row=i, column=10).value
-    size_height = sh.cell(row=i, column=11).value
-    features = sh.cell(row=i, column=12).value
-    feature_icon = sh.cell(row=i, column=13).value
-    """
-    cell_tier_price1 = cell_tier_price1.lower()
-    cell_tier_price2 = sh.cell(row=i, column=8).value
-    cell_tier_price3 = sh.cell(row=i, column=9).value
-    
-    if cell_tier_price == None:
-        continue
-    if type(cell_tier_price) == float:
-        pass
-    else:
-        cell_tier_price = float(cell_tier_price[1:])
-    """
+    name = sh.cell(row=i, column=4).value
+    size_range = sh.cell(row=i, column=6).value
+    size_fit = sh.cell(row=i, column=7).value
+    feature_icon = sh.cell(row=i, column=8).value
+    description = sh.cell(row=i, column=9).value
+    if description != None:
+        description = description.replace('\n',"")
+    fabric = sh.cell(row=i, column=10).value
+    feature = sh.cell(row=i, column=11).value
+    fabric_care = sh.cell(row=i, column=12).value
+    size_model = sh.cell(row=i, column=13).value
+    size_height = sh.cell(row=i, column=14).value
+    sleeve_length = sh.cell(row=i, column=16).value
+    fabric_type = sh.cell(row=i, column=17).value
+    garment_type = sh.cell(row=i, column=18).value
+    sub_brand = sh.cell(row=i, column=19).value
+    feature_color = sh.cell(row=i, column=20).value
     #value = [cell_tier_price, cell_tier_price*2]
     #print(value)
     """
@@ -67,7 +60,8 @@ for i in range(2, 120):
         features = ""
     """
     
-    new_list_value = [pro_slug, name, description, fabric, fabric_feature, fabric_care, size_range, size_fit, size_model, size_height, features, feature_icon]
+    new_list_value = [pro_slug, name, size_range, size_fit, feature_icon, description, fabric, feature, fabric_care, size_model, size_height, 
+                      sleeve_length, fabric_type, garment_type, sub_brand, feature_color]
     list1.append(new_list_value)
 
 #if you do want a unique list with no duplicates, remember to remove square brackets from above
--- jpeg_name_fix.py
+++ jpeg_name_fix.py
@@ -1,43 +1,65 @@
 # Import the os module
 import os
 
-# Get the current working directory
-cwd = os.getcwd()
+### IMPORTANT: Below are commands to check the working directory to see if you are in the correct one
 
-# Print the current working directory
-print("Current working directory: {0}".format(cwd))
+#------------------------------------------------------------------------
+# # Get the current working directory
+# cwd = os.getcwd()
+
+# # Print the current working directory
+# print("Current working directory: {0}".format(cwd))
+
+# # Another print the current working directory
+# print("Current working directory: {0}".format(os.getcwd()))
+
+# # Print the type of the returned object
+# print("os.getcwd() returns an object of type: {0}".format(type(cwd)))
+
+# # Change the current working directory. Need the "/" instead of "\"
+# os.chdir('C:/Work/Honeybee container/honeybee/products/management/data/')
+
+#------------------------------------------------------------------------
+
+print("------------------------------------------------------------------------")
+folder_path  = os.chdir('C:/Work/Honeybee container/honeybee/products/management/data/Image')
+
+# Print the folder_path
+print("Current working directory: {0}".format(os.getcwd()))
 
 # Print the type of the returned object
-print("os.getcwd() returns an object of type: {0}".format(type(cwd)))
+print("os.getcwd() returns an object of type: {0}".format(type(os.getcwd())))
 
-#------------------------------------------------------------------------
-
-# Print the current working directory
-print("Current working directory: {0}".format(os.getcwd()))
-
-# Change the current working directory. Need the "/" instead of "\"
-os.chdir('C:/Work/Honeybee container/honeybee/products/management/data/reshoot colour')
-
-# Print the current working directory
-print("Current working directory: {0}".format(os.getcwd()))
-
-#------------------------------------------------------------------------
-print("------------------------------------------------------------------------")
-folder_path  = os.chdir('C:/Work/Honeybee container/honeybee/products/management/data/reshoot colour')
-
+slug_list = []
 
 for file in os.listdir(folder_path):
-    file_name = file.split('.')[0].lower()
+    file_name = file.split('.')[0]
     file_name_list = file_name.split('_')
-    last_part = file_name_list[-1]
-    if len(file_name_list) >= 5:
-        print("These products have need of colour name fix " + file_name)
-    elif last_part == '01':
-        continue
-    elif last_part == '02':
-        continue
+    rename_item = file_name_list[1]
+    if rename_item.lower() == "talent":
+        replace_rename_item = "aTalent"
+    elif rename_item.lower() == "product":
+        replace_rename_item = "bPorduct"
     else:
-        print("These products have no 01 or 02 " + file_name)
+        continue
+    slug_list.append(file_name_list[0])
+    print("File name before replace: ", file)
+    os.rename(file, file.replace(rename_item, replace_rename_item))
+    print("File name after replace:  ", file.replace(rename_item, replace_rename_item))
+    print("-------------------------------------------")
+
+print(list(set(slug_list)))
+
+
+
+    # if len(file_name_list) >= 5:
+    #     print("These products have need of colour name fix " + file_name)
+    # elif last_part == '01':
+    #     continue
+    # elif last_part == '02':
+    #     continue
+    # else:
+    #     print("These products have no 01 or 02 " + file_name)
 
 
     
